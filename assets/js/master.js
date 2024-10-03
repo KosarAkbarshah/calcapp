4e46c40bd86e8dcb32155d9e9a93ffd7ccef2d7f
Danial Alborz <danialalborz3@gmail.com>
2024-10-03 15:41:03 -0700

    add student #1

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,3 +1,138 @@
+// danial's Js
+
+let studentOneForm = document.createElement('form');
+studentOneForm.id = "student1";
+studentOneForm.classList.add('form');
+document.querySelector("#student1").appendChild(studentOneForm);
+
+
+function assignmentCreator(number) {
+    let assignmentContainer = document.createElement('div');
+
+    // assignment input
+    let AssignmentInputContainer = document.createElement('div');
+    AssignmentInputContainer.id = `assignment-nu${number}-input`;
+    AssignmentInputContainer.classList.add('s2a1');
+
+    let AssignmentInputLabel = document.createElement('label');
+    AssignmentInputLabel.setAttribute('for', `Assignment-nu${number}-input`);
+    AssignmentInputLabel.textContent = `Assignment ${number}`;
+    AssignmentInputContainer.appendChild(AssignmentInputLabel);
+
+    let assignmentInput = document.createElement('input');
+    assignmentInput.type = 'text';
+    assignmentInput.name = `assignment-nu${number}`;
+    assignmentInput.className = 'grade-inputs';
+    assignmentInput.id = `Assignment-nu${number}-input`;
+    assignmentInput.placeholder = ' Mark';
+    assignmentInput.required = true;
+    AssignmentInputContainer.appendChild(assignmentInput);
+
+    let maxNumber = document.createElement('p');
+    maxNumber.style.display = 'inline-block';
+    maxNumber.textContent = '/30';
+    AssignmentInputContainer.appendChild(maxNumber);
+    // end of assignment input
+
+    // assignment output
+    let AssignmentOutputContainer = document.createElement('div');
+    AssignmentOutputContainer.id = `assignment-nu${number}-output`;
+    AssignmentOutputContainer.classList.add('s2a1mark');
+    studentOneForm.appendChild(AssignmentOutputContainer);
+
+    let AssignmentOutputLabel = document.createElement('label');
+    AssignmentOutputLabel.setAttribute('for', `Assignment-nu${number}-output`);
+    AssignmentOutputLabel.textContent = `Mark result #${number} `;
+    AssignmentOutputContainer.appendChild(AssignmentOutputLabel);
+
+    let assignmentOutput = document.createElement('input');
+    assignmentOutput.type = 'text';
+    assignmentOutput.name = `assignment-nu${number}-mark`;
+    assignmentOutput.className = 'grade-outputs';
+    assignmentOutput.id = `Assignment-nu${number}-output`;
+    assignmentOutput.placeholder = ' %';
+    assignmentOutput.disabled = true;
+    AssignmentOutputContainer.appendChild(assignmentOutput);
+    // end of assignment output
+
+    assignmentContainer.appendChild(AssignmentInputContainer);
+    assignmentContainer.appendChild(AssignmentOutputContainer);
+
+    return assignmentContainer;
+}
+
+let studentGradesLength = 3;
+
+for (let i = 0; i < studentGradesLength; i++) {
+    studentOneForm.appendChild(assignmentCreator(i + 1));
+}
+
+// submit btn 
+let submitContainer = document.createElement("div");
+submitContainer.className = "submitAv";
+studentOneForm.appendChild(submitContainer);
+
+let studentOneSubmitButton = document.createElement("input");
+studentOneSubmitButton.type = "submit";
+studentOneSubmitButton.value = "Calculate average";
+studentOneSubmitButton.className = "submit";
+studentOneSubmitButton.id = "submitbtn";
+submitContainer.appendChild(studentOneSubmitButton);
+// end of submit btn
+
+// average grade result
+let averageContainer = document.createElement("div");
+averageContainer.className = "student2avrage";
+studentOneForm.appendChild(averageContainer);
+
+let averageLabel = document.createElement("label");
+averageLabel.setAttribute("for", "s2avrage");
+averageContainer.appendChild(averageLabel);
+
+let averageInput = document.createElement("input");
+averageInput.type = "text";
+averageInput.id = "s2avrage";
+averageInput.placeholder = "%";
+averageInput.disabled = true;
+averageContainer.appendChild(averageInput);
+// end of average grade
+
+// calculate
+studentOneSubmitButton.addEventListener("click", function (event) {
+    event.preventDefault()
+
+    const gradeInputs = document.querySelectorAll(".grade-inputs");
+    const gradeOutputs = document.querySelectorAll(".grade-outputs");
+
+    let total = 0;
+    let count = 0;
+
+    for (let i = 0; i < gradeInputs.length; i++) {
+        const input = parseFloat(gradeInputs[i].value);
+        if (!isNaN(input) && input >= 0 && input <= 30) {
+            total += input;
+            count++;
+            gradeOutputs[i].placeholder = ((input / 30) * 100).toFixed(2) + "%";
+        } else if (input <= 0 || input >= 30) {
+            gradeOutputs[i].placeholder = "between 0-30";
+        } else if (isNaN(input)) {
+            gradeOutputs[i].placeholder = "please enter a number"
+        }
+    }
+
+    if (count > 0) {
+        const average = (total / count);
+        averageInput.value = ((average / 30) * 100).toFixed(2) + "%";
+    } else {
+        averageInput.value = "enter at least a number";
+    }
+});
+// end of calculate
+
+// End of danial's Js
+
+
+// farnaz
 const submitButton = document.getElementById('submit')
 
 const input1 = document.getElementById('s3a1inp')
@@ -32,3 +167,4 @@ submitButton.addEventListener('click', function (event) {
     document.getElementById('s3avrage').value = `${total} %`
 
 })
+// End of danial's Js
