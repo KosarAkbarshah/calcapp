58df812a36184fc99d091b9912d645d0ce1ff863
Tara niaki <taraniaki@gmail.com>
2024-10-10 05:46:07 -0400

    second push to main-merged

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,4 +1,4 @@
-// danial's Js
+// Danials js
 
 let studentOneForm = document.createElement('form');
 studentOneForm.id = "student1";
@@ -128,11 +128,84 @@ studentOneSubmitButton.addEventListener("click", function (event) {
     }
 });
 // end of calculate
+// end of Danials js
 
-// End of danial's Js
+///kosars js
+/// select  student 2 in forms
+const student2form = document.getElementById('student2')
+
+///select the inputs for assignments
+const s2a1inp = document.getElementById('s2a1inp')
+const s2a2inp = document.getElementById('s2a2inp')
+const s2a3inp = document.getElementById('s2a3inp')
+
+//// Select the fields of percentage marks
+const s2a1mark = document.getElementById('s2a1mark')
+const s2a2mark = document.getElementById('s2a2mark')
+const s2a3mark = document.getElementById('s2a3mark')
+
+///select button 
+const calculateBtn = document.getElementById('calcbtn');
+//select the avrage display field
+const avrageStudent2 = document.getElementById('s2avrage')
 
 
-// farnaz
+
+function calculateMarks(e) {
+
+    e.preventDefault(); // Prevent the form from refreshing the page
+
+    ///converting to numbers: should just accept numbers now
+    //.value refers to the current value entered by the user
+    const a1 = Number(s2a1inp.value);
+    const a2 = Number(s2a2inp.value);
+    const a3 = Number(s2a3inp.value);
+
+    //making sure the number is between 0-30
+    if (a1 >= 0 && a1 <= 30 && a2 >= 0 && a2 <= 30 && a3 >= 0 && a3 <= 30) {
+        // if condition applies, proceed with your calculation
+        const mark1s2 = (a1 / 30) * 100;
+        const mark2s2 = (a2 / 30) * 100;
+        const mark3s2 = (a3 / 30) * 100;
+
+        ///using tofixed to avoid decimal
+        s2a1mark.value = mark1s2.toFixed(2);
+        s2a2mark.value = mark2s2.toFixed(2);
+        s2a3mark.value = mark3s2.toFixed(2);
+
+        const avrage = (mark1s2 + mark2s2 + mark3s2) / 3;
+        avrageStudent2.value = avrage.toFixed(2)
+
+    }
+    else {
+        alert('numbers are bigger than 30 or smaller than 0');
+        resetFields();
+
+    }
+
+
+}
+
+
+// Function to reset all input and result fields
+function resetFields() {
+    s2a1inp.value = '';
+    s2a2inp.value = '';
+    s2a3inp.value = '';
+    s2a1mark.value = '';
+    s2a2mark.value = '';
+    s2a3mark.value = '';
+    averageStudent2.value = '';
+}
+
+
+// Attach the event listener to handle form submission
+student2form.addEventListener('submit', calculateMarks);
+
+///end of kosars js
+
+
+// farnaz js
 const submitButton = document.getElementById('submit')
 
 const input1 = document.getElementById('s3a1inp')
@@ -167,4 +240,5 @@ submitButton.addEventListener('click', function (event) {
     document.getElementById('s3avrage').value = `${total} %`
 
 })
-// End of danial's Js
+// End of farnaz's Js
+
